969853c38d34f6f1ad0c80aa89454c671f20cf3d
vivek22117 <vivekmishra22117@gmail.com>
2021-09-05 12:20:07 +0530

    adding new variable in the vars file

--- aws-eks-tf-modules/module.eks-cluster/data.tf
+++ aws-eks-tf-modules/module.eks-cluster/data.tf
@@ -5,7 +5,7 @@ data "terraform_remote_state" "eks_vpc" {
   backend = "s3"
 
   config = {
-    bucket = "${var.s3_bucket_prefix}-${var.environment}-${var.default_region}"
+    bucket = "${var.tfstate_s3_bucket_prefix}-tfstate-${var.default_region}"
     key    = "state/${var.environment}/eks-vpc/terraform.tfstate"
     region = var.default_region
   }
