a44d9c613abecca72742723f2434b94d710d8922
vivek22117 <vivekmishra22117@gmail.com>
2021-09-05 12:12:26 +0530

    adding TF code for EKS cluster module implementation

---
+++ deployment/eks-cluster/main.tf
@@ -0,0 +1,49 @@
+####################################################
+#        EKS Cluster module implementation         #
+####################################################
+module "eks-vpc" {
+  source = "../../aws-eks-tf-modules/module.eks-cluster"
+
+  environment    = var.environment
+  default_region = var.default_region
+
+  team         = var.team
+  owner        = var.owner
+  isMonitoring = var.isMonitoring
+
+  launch_template = var.launch_template
+
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  common_tags = var.common_tags
+
+  cluster_service_ipv4_cidr = var.cluster_service_ipv4_cidr
+  eks_cluster_create_timeout = var.eks_cluster_create_timeout
+  eks_cluster_delete_timeout = var.eks_cluster_delete_timeout
+  cluster_encryption_resources = var.cluster_encryption_resources
+  cluster_egress_cidrs = var.cluster_egress_cidrs
+
+  enable_private_access = var.enable_private_access
+  enable_public_access = var.enable_public_access
+  pvt_node_group_name     = var.pvt_node_group_name
+  pub_node_group_name     = var.pub_node_group_name
+  ami_type                = var.ami_type
+  disk_size               = var.disk_size
+  instance_types          = var.instance_types
+  pvt_desired_size        = var.pvt_desired_size
+  pvt_max_size            = var.pvt_max_size
+  pvt_min_size            = var.pvt_min_size
+  public_desired_size     = var.public_desired_size
+  public_max_size         = var.public_max_size
+  public_min_size         = var.public_min_size
+  log_retention           = var.log_retention
+  enabled_log_types       = var.enabled_log_types
+
+  node_labels = var.node_labels
+  ssh_public_key = var.ssh_public_key
+  ssh_source_sg = var.ssh_source_sg
+  vpc_cidr = var.vpc_cidr
+
+  tfstate_s3_bucket_prefix = var.tfstate_s3_bucket_prefix
+}
---
+++ deployment/eks-cluster/output.tf
@@ -0,0 +1,62 @@
+output "eks_cluster_id" {
+  description = "The name of the cluster"
+  value       = module.eks-vpc.eks_cluster_id
+}
+
+
+output "eks_cluster_arn" {
+  description = "The Amazon Resource Name (ARN) of the cluster"
+  value       = module.eks-vpc.eks_cluster_arn
+}
+
+output "eks_cluster_endpoint" {
+  value = module.eks-vpc.eks_cluster_endpoint
+}
+
+output "eks_cluster_version" {
+  description = "The Kubernetes server version of the cluster"
+  value       = module.eks-vpc.eks_cluster_version
+}
+
+output "eks_cluster_identity_oidc_issuer" {
+  description = "The OIDC Identity issuer for the cluster"
+  value       = module.eks-vpc.eks_cluster_identity_oidc_issuer
+}
+
+output "eks_cluster_identity_oidc_issuer_arn" {
+  description = "The OIDC Identity issuer ARN for the cluster that can be used to associate IAM roles with a service account"
+  value       = module.eks-vpc.eks_cluster_identity_oidc_issuer_arn
+}
+
+output "eks_cluster_identity_oidc_url" {
+  value = module.eks-vpc.eks_cluster_identity_oidc_url
+}
+
+output "eks_cluster_iam_role_arn" {
+  value = module.eks-vpc.eks_cluster_iam_role_arn
+}
+
+output "eks_cluster_worker_role_arn" {
+  value = module.eks-vpc.eks_cluster_worker_role_arn
+}
+
+output "eks_cluster_certificate_authority" {
+  value = module.eks-vpc.eks_cluster_certificate_authority
+}
+
+output "eks_cluster_austoscaling_policy_arn" {
+  value = module.eks-vpc.eks_cluster_austoscaling_policy_arn
+}
+
+
+output "eks_vpc_config" {
+  value = module.eks-vpc.eks_vpc_config
+}
+
+output "eks_cluster_sg_id" {
+  value = module.eks-vpc.eks_cluster_sg_id
+}
+
+output "worker_node_ssh_key" {
+  value = module.eks-vpc.worker_node_ssh_key
+}
