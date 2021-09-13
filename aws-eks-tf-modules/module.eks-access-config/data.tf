cc9bc77758f0c6d4d2dab497180b713f0e55690a
vivek22117 <vivekmishra22117@gmail.com>
2021-09-13 12:22:54 +0530

    adding data TF file to read remote state of EKS VPC and EKS cluster

---
+++ aws-eks-tf-modules/module.eks-access-config/data.tf
@@ -0,0 +1,55 @@
+###########################################################
+#             Remote state configuration to fetch         #
+#                  eks vpc, artifactory bucket            #
+###########################################################
+data "terraform_remote_state" "eks_vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.s3_bucket_prefix}-tfstate-${var.default_region}"
+    key    = "state/${var.environment}/eks-vpc/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+
+data "terraform_remote_state" "eks_cluster" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.s3_bucket_prefix}-tfstate-${var.default_region}"
+    key    = "state/${var.environment}/eks-cluster/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+data "terraform_remote_state" "s3_buckets" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.s3_bucket_prefix}-tfstate-${var.default_region}"
+    key    = "state/${var.environment}/s3-buckets/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+data "template_file" "eks_read_only_template" {
+  template = file("${path.module}/policy-doc/eks-full-access.json.tpl")
+}
+
+data "template_file" "eks_full_access_template" {
+  template = file("${path.module}/policy-doc/eks-read-access.json.tpl")
+}
+
+data "template_file" "eks_admin_host_user_data" {
+  template = file("${path.module}/data-scripts/configure-eks.sh")
+
+  vars = {
+    eks_create_role_arn     = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/eks-creator"
+    cluster_name            = data.terraform_remote_state.eks_cluster.outputs.eks_cluster_id
+    default_region          = var.default_region
+    artifactory_bucket_name = data.terraform_remote_state.s3_buckets.outputs.artifactory_s3_name
+  }
+}
+
+data "aws_caller_identity" "current" {}
