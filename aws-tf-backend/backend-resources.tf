3e078171cc241528828ea838bed95026236c965b
vivek22117 <vivekmishra22117@gmail.com>
2021-09-14 13:13:56 +0530

    adding TF providers file

--- aws-tf-backend/backend-resources.tf
+++ aws-tf-backend/backend-resources.tf
@@ -1,6 +1,6 @@
 #####=====================Terraform tfstate backend S3===================#####
 resource "aws_s3_bucket" "tf_state_backend_bucket" {
-  bucket = "${var.environment}-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+  bucket = "${var.environment}-eks-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
   acl    = "private"
 
   force_destroy = true
@@ -37,7 +37,7 @@ resource "aws_s3_bucket" "tf_state_backend_bucket" {
 
 #####=========================DynamoDB Table for tfstate state lock=====================#####
 resource "aws_dynamodb_table" "dynamodb-terraform-state-lock" {
-  name           = "${var.environment}-tfstate-lock-db-${var.default_region}"
+  name           = "${var.environment}-eks-tfstate-lock-db-${var.default_region}"
   read_capacity  = 2
   write_capacity = 2
 
