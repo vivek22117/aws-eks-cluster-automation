f0deca5b1e6f15eae107c97a2002f817c5aea9e4
vivek22117 <vivekmishra22117@gmail.com>
2021-09-19 07:08:04 +0530

    formatting TF script for EKS module

--- deployment/eks-access-config/main.tf
+++ deployment/eks-access-config/main.tf
@@ -7,32 +7,32 @@ module "eks_access_infra" {
   environment    = var.environment
   default_region = var.default_region
 
-  team = var.team
-  owner = var.owner
-  project = var.project
+  team         = var.team
+  owner        = var.owner
+  project      = var.project
   isMonitoring = var.isMonitoring
-  component = var.component
+  component    = var.component
 
   eks_bastion_name_prefix = var.eks_bastion_name_prefix
   bastion_instance_type   = var.bastion_instance_type
   default_cooldown        = var.default_cooldown
-  volume_size = var.volume_size
+  volume_size             = var.volume_size
 
   eks_bastion_asg_desired_capacity = var.eks_bastion_asg_desired_capacity
-  eks_bastion_asg_max_size = var.eks_bastion_asg_max_size
-  eks_bastion_asg_min_size = var.eks_bastion_asg_min_size
-  termination_policies = var.termination_policies
+  eks_bastion_asg_max_size         = var.eks_bastion_asg_max_size
+  eks_bastion_asg_min_size         = var.eks_bastion_asg_min_size
+  termination_policies             = var.termination_policies
 
   eks_iam_group = var.eks_iam_group
 
 
-  apply_config_map_aws_auth = var.apply_config_map_aws_auth
+  apply_config_map_aws_auth   = var.apply_config_map_aws_auth
   map_additional_aws_accounts = var.map_additional_aws_accounts
-  map_additional_iam_roles = var.map_additional_iam_roles
-  map_additional_iam_users = var.map_additional_iam_users
+  map_additional_iam_roles    = var.map_additional_iam_roles
+  map_additional_iam_users    = var.map_additional_iam_users
 
-  kubeconfig_path = var.kubeconfig_path
+  kubeconfig_path              = var.kubeconfig_path
   configmap_auth_template_file = var.configmap_auth_template_file
-  configmap_auth_file = var.configmap_auth_file
+  configmap_auth_file          = var.configmap_auth_file
 
 }
