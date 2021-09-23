ed92975de99eb459a7c1afc37c484bffe91d8c71
vivek22117 <vivekmishra22117@gmail.com>
2021-09-23 07:30:53 +0530

    adding variables file

---
+++ aws-eks-tf-modules/module.eks-irsa/variables.tf
@@ -0,0 +1,95 @@
+######################################################################
+# Global variables for VPC, Subnet, Routes and Bastion Host          #
+######################################################################
+variable "default_region" {
+  type        = string
+  description = "AWS region to deploy resources"
+}
+
+
+######################################################
+# Local variables defined                            #
+######################################################
+variable "team" {
+  type        = string
+  description = "Owner team for this application infrastructure"
+}
+
+variable "owner" {
+  type        = string
+  description = "Owner of the product"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment to be used"
+}
+
+variable "isMonitoring" {
+  type        = bool
+  description = "Monitoring is enabled or disabled for the resources creating"
+}
+
+variable "project" {
+  type        = string
+  description = "Monitoring is enabled or disabled for the resources creating"
+}
+
+variable "component" {
+  type        = string
+  description = "Component name for the resource"
+}
+
+
+#####=============Local variables===============#####
+locals {
+  common_tags = {
+    Owner       = var.owner
+    Team        = var.team
+    Environment = var.environment
+    Monitoring  = var.isMonitoring
+    Project     = var.project
+    Component   = var.component
+  }
+}
+
+#####========================IRSA Config Variables====================#####
+variable "enabled" {
+  type        = bool
+  description = "A conditional indicator to enable"
+}
+
+variable "namespace" {
+  type        = string
+  description = "The namespace where kubernetes service account is, example 'kube-system'"
+}
+
+variable "service_account" {
+  type        = string
+  description = "The name of kubernetes service account, example 'aws-node'"
+}
+
+variable "policy_arns" {
+  description = "A list of policy ARNs to attach the role"
+  type        = list(string)
+  default     = []
+}
+
+variable "oidc_url" {
+  description = "A URL of the OIDC Provider"
+  type        = string
+}
+
+variable "oidc_arn" {
+  description = "An ARN of the OIDC Provider"
+  type        = string
+}
+
+
+variable "name" {
+  description = "The logical name of the module instance"
+  type        = string
+  default     = null
+}
+
+
