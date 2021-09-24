f0b8c035506168a11fd50e676b614ba5f313908e
vivek22117 <vivekmishra22117@gmail.com>
2021-09-24 22:48:14 +0530

    adding TF script to implement IRSA module

---
+++ deployment/eks-irsa/main.tf
@@ -0,0 +1,20 @@
+####################################################
+#        EKS Access Infra module configuration     #
+####################################################
+module "eks_irsa" {
+  source = "../../aws-eks-tf-modules/module.eks-irsa"
+
+  component      = var.component
+  environment    = var.environment
+  default_region = var.default_region
+  isMonitoring   = var.isMonitoring
+  project        = var.project
+  owner          = var.owner
+  team           = var.team
+
+  enabled         = var.enabled
+  namespace       = var.namespace
+  oidc_arn        = var.oidc_arn
+  oidc_url        = var.oidc_url
+  service_account = var.service_account
+}
---
+++ deployment/eks-irsa/test.tfvars
@@ -0,0 +1,16 @@
+default_region = "us-east-1"
+
+team         = "LearningTeam"
+owner        = "Vivek"
+isMonitoring  = true
+project     = "LearningTF"
+component = "Managed-EKS-IRSA"
+
+enabled = true
+namespace = "kube-system"
+service_account = "aws-node"
+policy_arns = []
+oidc_url = ""
+oidc_arn = ""
+name = null
+
