3098e218f9992ead5723e48b9bde906c92b9ddf8
vivek22117 <vivekmishra22117@gmail.com>
2021-09-28 19:35:45 +0530

    updating AMI filter script

--- aws-eks-tf-modules/module.eks-access-config/ami.tf
+++ aws-eks-tf-modules/module.eks-access-config/ami.tf
@@ -1,12 +1,22 @@
 ####################################################
-#             Bastion host AMI for EKS             #
+#             EKS Admin host AMI for EKS           #
 ####################################################
+locals {
+  ami_filter_prefix = var.ami_filter_type == "self" ? "eks-admin-*" : "amzn2-ami-*-x86_64-gp2"
+}
+
+
 data "aws_ami" "eks_admin_host" {
-  owners      = ["self"]
   most_recent = true
+  owners      = [var.ami_filter_type]
 
   filter {
     name   = "name"
-    values = ["eks-admin-"]
+    values = [local.ami_filter_prefix]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
