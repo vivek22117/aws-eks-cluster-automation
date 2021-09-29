a58bb6eaa4465af667cbf05ad676c84bb9d8945b
vivek22117 <vivekmishra22117@gmail.com>
2021-09-29 08:21:57 +0530

    adding TF data script to fetch EKS VPC and EKS Cluster state file

---
+++ aws-eks-tf-modules/module.eks-cluster-autoscaler/data.tf
@@ -0,0 +1,27 @@
+###########################################################
+#             Remote state configuration to fetch         #
+#                  eks vpc, eks cluster state file        #
+###########################################################
+data "terraform_remote_state" "eks_vpc" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-eks-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/eks-vpc/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+
+data "terraform_remote_state" "eks_cluster" {
+  backend = "s3"
+
+  config = {
+    bucket = "${var.environment}-eks-tfstate-${data.aws_caller_identity.current.account_id}-${var.default_region}"
+    key    = "state/${var.environment}/eks-cluster/terraform.tfstate"
+    region = var.default_region
+  }
+}
+
+
+data "aws_caller_identity" "current" {}
