7a0ede2ed59f767a107e256e6d7b3da035a5d0e0
vivek22117 <vivekmishra22117@gmail.com>
2021-10-01 10:39:07 +0530

    updating local deployment script

--- local-deployment-script.sh
+++ local-deployment-script.sh
@@ -229,6 +229,27 @@ function deploy_eks_container_insights_resources() {
 }
 
 
+function deploy_eks_cluster_autoscaler_resources() {
+  echo -e "\n\n ========================= Starting EKS cluster autoscaler resources deployment using TF ====================="
+
+  cd deployment/eks-cluster-autoscaler
+
+  sed -i '/profile/s/^#//g' providers.tf
+  sed -i "s/us-east-1/$AWS_REGION/g" providers.tf
+  sed -i "s/us-east-1/$AWS_REGION/g" config/$ENV-backend-config.config
+
+  terraform init -backend-config="config/$ENV-backend-config.config" \
+  -backend-config="bucket=$ENV-tfstate-$AWS_ACCOUNT_ID-$AWS_REGION" -reconfigure
+
+  terraform plan -var-file="$ENV.tfvars" -var="default_region=$AWS_REGION" -var="environment=$ENV"
+  terraform apply -var-file="$ENV.tfvars" -var="default_region=$AWS_REGION" -var="environment=$ENV" -auto-approve
+
+  cd ../..
+
+  echo -e "============================== Completed ================================================ \n\n"
+}
+
+
 
 
 if [ $EXEC_TYPE == 'apply' ]; then
@@ -292,6 +313,18 @@ if [ $EXEC_TYPE == 'apply' ]; then
 
   if [ $ENABLE == 'Yes' ]; then
     deploy_eks_container_insights_resources
+  fi
+
+
+    PS3="Do you want to deploy EKS Cluster Autoscaler resources? It will create PODs in the Cluster"
+  select ENABLE in Yes No
+  do
+    echo "You decision is $ENABLE to deploy EKS Cluster Autoscaler resources!"
+    break
+  done
+
+  if [ $ENABLE == 'Yes' ]; then
+    deploy_eks_cluster_autoscaler_resources
   fi
 
 
