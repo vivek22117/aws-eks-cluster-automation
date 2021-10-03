e62fb2c2a0cb575cd3dbfa42e31f69260c71c8dd
vivek22117 <vivekmishra22117@gmail.com>
2021-10-03 14:44:32 +0530

    updating eks infra pipeline

--- aws-eks-tf-modules/module.eks-access-config/data-scripts/configure-eks.sh
+++ aws-eks-tf-modules/module.eks-access-config/data-scripts/configure-eks.sh
@@ -1,9 +1,53 @@
 #!/usr/bin/env bash
 
 
+sudo yum update -y
+sudo yum install wget unzip -y
+sleep 5
 
 sudo yum install jq -y
 cd /tmp
+
+
+if [ ${ami_filter_type}=='amazon' ]; then
+
+  echo "Installing AWS CLI v2"
+  curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+  unzip awscliv2.zip
+  sudo  ./aws/install -i /usr/local/aws-cli -b /usr/local/bin
+
+  echo "Install SSM-Agent"
+  sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+  sudo systemctl enable amazon-ssm-agent
+  sudo systemctl start amazon-ssm-agent
+
+
+  echo "Installing Kubectl"
+  # https://docs.aws.amazon.com/eks/latest/userguide/install-kubectl.html
+  curl -o kubectl https://amazon-eks.s3.us-west-2.amazonaws.com/${kubectl_version}/2021-01-05/bin/linux/amd64/kubectl
+  chmod +x ./kubectl
+  mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
+
+  kubectl version --short --client
+
+
+  echo "Installing eksctl"
+  curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+  mv /tmp/eksctl /usr/local/bin
+
+
+  echo "Install Terraform"
+  wget https://releases.hashicorp.com/terraform/${terraform_version}/terraform_${terraform_version}_linux_amd64.zip
+  unzip terraform_${terraform_version}_linux_amd64.zip
+  mv terraform /usr/local/bin/
+  terraform version
+
+
+  echo "Install HELM"
+  curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+fi
+
+
 
 function download_eks_auth() {
   echo ================== Download EKS auth config ===============================
