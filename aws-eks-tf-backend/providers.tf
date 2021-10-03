e49a6671e148f2697160e129336b58528ae306b6
vivek22117 <vivekmishra22117@gmail.com>
2021-10-03 15:01:59 +0530

    updating eks infra pipeline

--- aws-eks-tf-backend/providers.tf
+++ aws-eks-tf-backend/providers.tf
@@ -13,11 +13,6 @@ terraform {
 
   required_version = ">= 0.13"
 
-  backend "s3" {
-    #    profile        = "default"
-    region  = "us-east-1"
-    encrypt = "true"
-  }
 }
 
 data "aws_caller_identity" "current" {}
